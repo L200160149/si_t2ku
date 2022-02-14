b204d545f8b35b6f037671150c24c4000bb4b505
L200160149 <ums.informatika@gmail.com>
2022-02-14 15:58:34 +0700

    view pegawai

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,10 +2,19 @@ module.exports = {
     viewDashboard: async (req, res) => {
         try {
             res.render("admin/dashboard/view_dashboard", {
-                title: 'T2KU BMCK Jateng | Dashboard'
+                title: 'Dashboard | T2KU BMCK Jateng'
             })
         } catch (error) {
             console.log(error)
         }
-    }
+    },
+    viewPegawai: async (req, res) => {
+        try {
+            res.render("admin/pegawai/view_pegawai", {
+                title: 'Data Pegawai | T2KU BMCK Jateng'
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    },
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,7 @@ const adminController = require('../controllers/adminController');
 
 // route dashboard
 router.get('/dashboard', adminController.viewDashboard);
+// route pegawai
+router.get('/pegawai', adminController.viewPegawai);
 
 module.exports = router;
